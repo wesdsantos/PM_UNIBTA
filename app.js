bf9337590f7b41bf48a3e922c16d62457331639b
wesley dias <dias.wsantos@gmail.com>
2024-10-31 15:19:51 -0300

    projeto

---
+++ app.js
@@ -0,0 +1,20 @@
+// function logar(){
+
+//   const login = document.getElementById("login").value;
+//   const senha = document.getElementById("senha").value;
+
+//   if(login === "admin" && senha === "admin"){
+//       alert("sucesso");
+//       location.href = index.html;
+        
+//   }else{
+//     alert("Usuário ou senha incorretos");
+
+//   }
+
+// }
+
+ 
+// document.getElementById("buttonRoulette").addEventListener("click", function() {
+//   window.location.href = "pagina_roulette.html"; 
+// });
